61c2bbeebd465877e1fca01337f99aa57031d611
Fredrik03 <fredrikds03@gmail.com>
2025-05-20 13:33:54 +0200

    Added slowapi to the requirements.txt

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os, secrets
 
 BUYIN_DEFAULT   = 100
 SESSION_SECRET  = os.getenv("SESSION_SECRET") or secrets.token_hex(32)
-DATABASE_URL    = "poker.db"
+DATABASE_URL    = "data/poker.db"
 
 # Bootstrap admin account:
 ADMIN_USERNAME  = os.getenv("ADMIN_USERNAME", "admin")
